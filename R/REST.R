6b985b50c0b53495c7bb857024baf764e2f25c3f
Martin Morgan <martin.morgan@roswellpark.org>
2016-06-08 13:03:53 -0400

    update status() for parameters

--- R/REST.R
+++ R/REST.R
@@ -1,10 +1,9 @@
 #' (internal) GET endpoint / uri
 #'
 #' @importFrom httr GET stop_for_status
-.gdc_get <- function(endpoint, query=NULL, ..., base=.gdc_base) {
+.gdc_get <- function(endpoint, parameters=list(), ..., base=.gdc_base) {
     stopifnot(is.character(endpoint), length(endpoint) == 1L)
-    uri <- sprintf("%s%s%s", base, endpoint,
-                   if (!is.null(query)) sprintf("?%s", query) else "")
+    uri <- sprintf("%s/%s%s", base, endpoint, .parameter_string(parameters))
     response <- GET(uri, ...)
     stop_for_status(response)
     response
--- R/status.R
+++ R/status.R
@@ -3,7 +3,9 @@
 #' @param version (optional) character(1) version of GDC
 #'
 #' @return List describing current status.
+#' 
 #' @importFrom httr content
+#'
 #' @export
 status <- function(version=NULL) {
     response <- .gdc_get(paste(version, "status", sep="/"))
