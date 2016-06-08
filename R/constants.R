5e6d1f5ce39338726c8440afc1bc2245ae6017af
Martin Morgan <martin.morgan@roswellpark.org>
2016-06-08 13:03:19 -0400

    add parameters constants

--- R/constants.R
+++ R/constants.R
@@ -3,11 +3,39 @@
     c("status", "projects", "cases", "files", "annotations", "data",
       "manifest", "slicing", "submission")
 
+.gdc_parameters <-
+    list(format="JSON", pretty=FALSE, fields=NULL, size=10L, from=1L,
+         sort=NULL, filters=NULL, facets=NULL)
+
+.constants_format <- function(names) {
+    txt <- strwrap(paste(names, collapse=", "), indent=4, exdent=4)
+    paste(txt, collapse="\n")
+}
+
 #' Print available endpoints
 #'
 #' @rdname constants
 #' @export
-endpoints <- function() {
-    txt <- strwrap(paste(.gdc_endpoint, collapse=", "), indent=4, exdent=4)
-    cat("available endpoints:", paste(txt, collapse="\n"), sep="\n")
+endpoints <- function()
+    cat("available endpoints:", .constants_format(.gdc_endpoint), sep="\n")
+
+#' Print available parameters
+#'
+#' @rdname constants
+#' @export
+parameters <- function()
+    cat("available parameters:", .constants_format(names(.gdc_parameters)),
+        sep="\n")
+
+.parameter_string <- function(parameters) {
+    if (is.null(parameters))
+        return("")
+    stopifnot(is.list(parameters),
+              all(names(parameters) %in% names(.gdc_parameters)))
+
+    default <- .gdc_parameters
+    default[names(parameters)] <- parameters
+    default <- Filter(Negate(is.null), default)
+    string <- paste(names(default), unname(default), sep="=", collapse="&")
+    sprintf("?%s", string)
 }
