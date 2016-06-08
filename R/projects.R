4bb608e6c26e2cb9c1fb898a2143a5f80125a6bf
Martin Morgan <martin.morgan@roswellpark.org>
2016-06-08 13:43:54 -0400

    projects(): allow all fields

--- R/projects.R
+++ R/projects.R
@@ -1,6 +1,27 @@
-.project_fields <- 
+.project_all_fields <-
+    c("dbgap_accession_number", "disease_type", "name",
+      "primary_site", "program.dbgap_accession_number",
+      "program.name", "program.program_id", "project_id", "released",
+      "state", "summary.case_count",
+      "summary.data_categories.case_count",
+      "summary.data_categories.data_category",
+      "summary.data_categories.file_count",
+      "summary.experimental_strategies.case_count",
+      "summary.experimental_strategies.experimental_strategy",
+      "summary.experimental_strategies.file_count",
+      "summary.file_count", "summary.file_size")    
+
+.project_primary_fields <- 
     c("dbgap_accession_number", "disease_type", "released", "state",
       "primary_site", "project_id", "name")
+
+#' @export
+project_fields <- function(primary=TRUE) {
+    if (primary)
+        .project_primary_fields
+    else
+        .project_all_fields
+}
 
 #' Query GDC for projects
 #'
@@ -17,8 +38,13 @@
 #' dim(df)
 #' 
 #' @export
-projects <- function(...) {
-    response <- .gdc_get("projects", parameters=list(format="XML", ...))
+projects <- function(..., fields=project_fields())
+{
+    stopifnot(all(fields %in% project_fields(primar=FALSE)))
+    fields0 <- paste(fields, collapse=",")
+
+    response <- .gdc_get(
+        "projects", parameters=list(format="XML", fields=fields0, ...))
     xml <- content(response, type="application/xml")
 
     warnings <- as.character(xml_find_all(xml, "/response/warnings/text()"))
@@ -26,12 +52,18 @@ projects <- function(...) {
         warning("'projects' query warnings:\n",
                 paste(strwrap(warnings, indent=4, exdent=4), collapse="\n"))
 
-    xpaths <- setNames(sprintf("/response/data/hits/item/%s", .project_fields),
-                       .project_fields)
-    fields <- lapply(xpaths, function(xpath) {
+    xpaths <- setNames(sprintf("/response/data/hits/item/%s", fields), fields)
+    columns <- lapply(xpaths, function(xpath) {
         nodes <- xml_find_all(xml, xpath)
         vapply(nodes, xml_text, character(1))
     })
+    columns <- Filter(length, columns)
 
-    as.data.frame(fields, stringsAsFactors=FALSE)
+    dropped <- fields[!fields %in% names(columns)]
+    if (length(dropped))
+        warning("fields not available:\n",
+                paste(strwrap(paste(dropped, collapse=", "), indent=4, exdent=4),
+                      "\n"))
+
+    as.data.frame(columns, stringsAsFactors=FALSE)
 }
