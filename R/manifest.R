18565f9863523404d59975e7a435cce12456040c
Martin Morgan <martin.morgan@roswellpark.org>
2016-06-10 17:25:30 -0400

    implement manifest() and transfer()

---
+++ R/manifest.R
@@ -0,0 +1,81 @@
+#' GDC manifest file creation and file download
+#'
+#' \code{manifest()} creates a manifest of files to be downloaded
+#' using the GDC Data Transfer Tool.
+#' 
+#' @param uuids character() of GDC file UUIDs.
+#'
+#' @param destination_dir character(1) file path to a directory for
+#'     downloading the manifest or files. The manifest file name, of
+#'     the form \sQuote{gdc_manifest_20160610_12345.txt}, must not
+#'     exist.
+#'
+#' @examples
+#' uuids <- c("e3228020-1c54-4521-9182-1ea14c5dc0f7",
+#'            "18e1e38e-0f0a-4a0e-918f-08e6201ea140")
+#' (manifest <- manifest(uuids))
+#' @export
+manifest <- function(uuids, destination_dir=tempfile()) {
+    stopifnot(is.character(uuids))
+    .dir_validate_or_create(destination_dir)
+    endpoint <- "manifest"
+
+    uuids <- trimws(uuids)
+    uri <- sprintf("%s/%s", endpoint, paste(uuids, collapse=","))
+    destination <- tempfile(tmpdir=destination_dir)
+
+    .gdc_download_one(uri, destination, overwrite=FALSE, progress=FALSE)
+}
+
+#' \code{transfer()} retrieves files from the manifest using the GDC
+#' Data Transfer Tool.
+#'
+#' @param manifest character(1) file path to manifest created by
+#'     \code{manifest()}
+#'
+#' @param args character() vector specifying command-line arguments to
+#'     be passed to \code{gdc-client}. See \code{transfer_help} for
+#'     possible values. The arguments \code{--manifest} and
+#'     \code{--dir} are determined by \code{manifest} and
+#'     \code{destination_dir}, respectively, and should NOT be
+#'     provided.
+#'
+#' @param gdc_client character(1) name or path to \code{gdc-client}
+#'     executable. On Windows, use \code{/} or \code{\\\\} as the file
+#'     separator.
+#'
+#' @return character(1) directory path to which the files were
+#'     downloaded.
+#' 
+#' @examples
+#' \donttest{
+#' destination <- transfer(manifest)
+#' dir(destination)
+#' }
+#' @rdname manifest
+#' @export
+transfer <-
+    function(manifest, destination_dir=tempfile(), args=character(),
+             gdc_client="gdc-client")
+{
+    stopifnot(is.character(manifest), length(manifest) == 1L,
+              file.exists(manifest))
+    .dir_validate_or_create(destination_dir)
+
+    dir <- sprintf("--dir %s", destination_dir)
+    manifest <- sprintf("--manifest %s", manifest)
+    args <- paste(c("download", dir, manifest, args), collapse=" ")
+    system2("gdc-client", args)
+
+    destination_dir
+}
+
+#' \code{transfer_help()} queries the the command line GDC Data
+#' Transfer Tool, \code{gdc-client}, for available options to be used
+#' in the \code{transfer()} command.
+#' 
+#' @rdname manifest
+#' @export
+transfer_help <- function(gdc_client="gdc-client") {
+    system2(gdc_client, "download -h")
+}
