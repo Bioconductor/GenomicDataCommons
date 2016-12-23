bb4a5f247a52c38ce3e97c861f96e0276417d55c
Sean Davis <seandavi@gmail.com>
2016-12-22 20:49:01 -0500

    Filters working, but still need cleanup

--- R/filters.R
+++ R/filters.R
@@ -1,58 +1,52 @@
-.filterOps = c('&'  = 'and',
-               '==' = '=',
-               '!=' = '!=',
-               '>=' = '>=',
-               '<=' = '<=',
-               '>'  = '>',
-               '<'  = '<',
-               '%is%' = 'is',
-               '!'  = 'not',
-               '|'  = 'or',
-               '%in%' = 'in',
-               '%exclude%' = 'exclude')
-
-.dropDot = function(s) {
-  return(strsplit(s,'\\.')[1])
+.unary_op <- function(left) {
+  force(left)
+  function(e1) {
+    force(e1)
+    list(op=e1,content=c(field=left,value=c(right)))
+  }
 }
 
-.makeOp = function(op) {
-  force(.makeOp)
-  if(op %in% c('==','!=','>=','<=','>','<','%in%','%exclude%','%is%')) {
-    return(function(x,y) {
-      return(list(op=jsonlite::unbox(.filterOps[op]),
-              content=list(field=jsonlite::unbox(x),value=y)))
-    })
+.binary_op <- function(sep) {
+  force(sep)
+  function(e1, e2) {
+      force(e1)
+      force(e2)
+      list(op=unbox(sep),content=list(field=e1,value=e2))
   }
-  if(op=='(') {
-    return(function(x) {
-      return(list(x))
-    })
-  }
-  if(op %in% c('&','|')) {
-    return(function(x,y) {
-      return(list(op=jsonlite::unbox(.filterOps[op]),
-                  content=list(x,y)))
-    })
-  }
-  if(class(op)=='lazy') {
-    return(lazy_eval(op))
-  }
-  return(function(op) {
-    return(as.character(op))
-  })
 }
 
-.endpoints = c('projects','files','cases','annotations')
+combine_op = function(sep) {
+  force(sep)
+  function(e1, e2) {
+      force(e1)
+      force(e2)
+    return(list(op=unbox(sep),content=list(e1,e2)))
+  }
+}
+
+.f_env = new.env(parent=emptyenv())
+# .f_env=list()
+.f_env$`==` = .binary_op('=')
+.f_env$`!=` = .binary_op('!=')
+.f_env$`<` = .binary_op('<')
+.f_env$'>' = .binary_op('>')
+.f_env$'&' = combine_op('and')
+.f_env$'|' = combine_op('or')
+.f_env$`<=` = .binary_op('<=')
+.f_env$'>=' = .binary_op('>=')
+.f_env$'%in%' = .binary_op('in')
+.f_env$'%exclude%' = .binary_op('exclude')
+
 
 #' Create GDC filters
 #'
 #' Create filters using standard R formatting
 #'
 #' This function makes arbitrarily complex filter functions for
-#' and creates translated JSON for use in queries of the GDC.
+#' and creates a list that is ready for easy conversion with
+#' toJSON for use in queries of the GDC.
 #'
-#' @param filter_expression an R expression
-#' @param asJSON if TRUE, return the JSON.  If FALSE, return an R list
+#' @param filter_expression an R-like expression
 #' @param endpoint one of the recognized endpoints; used to pull accepted fields for
 #'   use in query construction
 #' @param ... passed to \code{jsonlite::toJSON}
@@ -61,24 +55,14 @@
 #' @importFrom jsonlite toJSON
 #'
 #' @examples
-#' filters(diagnoses.age_at_diagnosis <= 10*365,endpoint='cases')
-#' filters(project.primary_site %in% c('blood','brain'),endpoint='cases')
+#' #filter_expr(diagnoses.age_at_diagnosis <= 10*365,endpoint='cases')
+#' #filter_expr(project.primary_site %in% c('blood','brain'),endpoint='cases')
 #' @export
-filters = function(filter_expression, endpoint, asJSON=TRUE, ...) {
-  if(!(endpoint %in% .endpoints)) {
-    stop(sprintf('endpoint must be one of %s',paste(.endpoints,sep=",")))
-  }
-  ca = match.call()
-  ops = sapply(names(.filterOps),.makeOp)
-  fields = mapping(endpoint)$fields
-  names(fields) = fields
-  ops = c(ops,fields)
-  ## The eval and match.call combo keep R from
-  ##   evaluating until we are in the right context 
-  ##   (i.e., with ops in place)
-  return(lazy(with(ops,eval(ca$filter_expression))))
-  if(asJSON) {
-    return(jsonlite::toJSON(retlist,...))
-  }
-  return(retlist)
+filter_expr = function(expr,available_fields,endpoint) {
+    x = substitute(expr)
+    available_fields=list(available_fields)
+    names(available_fields)=available_fields
+    filt_env = list2env(as.list(.f_env),parent=list2env(available_fields))
+    eval(x,filt_env,enclos = parent.frame())
 }
+    
