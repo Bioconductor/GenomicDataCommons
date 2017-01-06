a95b23b6b6fba9803f14250ed02b5f35c4149f93
Sean Davis <seandavi@gmail.com>
2017-01-06 16:28:29 -0500

    Mapping endpoint returns data.frame
    Fixes #17

--- R/mapping.R
+++ R/mapping.R
@@ -10,21 +10,35 @@
 #'     include \dQuote{projects}, \dQuote{cases}, \dQuote{files}, and
 #'     \dQuote{annotations}.
 #'
-#' @param token (optional) character(1) security token allowing access
-#'     to restricted data. See
-#'     \url{https://docs.gdc.cancer.gov/API/Users_Guide/Getting_Started/#authentication}.
-#' 
+#' @return A data frame describing the field (field name), full (full
+#'     data model name), type (data type), and four additional columns
+#'     describing the "set" to which the fields belong--\dQuote{default},
+#'     \dQuote{expand}, \dQuote{multi}, and \dQuote{nested}.
+#'
 #' @examples
 #' map <- mapping("projects")
-#' map["default"]
+#' head(map)
+#' # get only the "default" fields
+#' subset(map,defaults)
+#' # And get just the text names of the "default" fields
+#' subset(map,defaults)$field
+#' 
 #' @importFrom httr content
 #' @export
-mapping <- function(endpoint, token=NULL) {
+mapping <- function(endpoint) {
     valid <- c("projects", "cases", "files", "annotations")
     stopifnot(is.character(endpoint), length(endpoint) == 1L,
               endpoint %in% valid)
     response <- .gdc_get(
-        sprintf("%s/%s", endpoint, "_mapping"), token=token)
+        sprintf("%s/%s", endpoint, "_mapping"))
     json <- content(response, type="application/json")
-    .response_mapping_as_list(json)
+    df = do.call(rbind,json[['_mapping']])
+    tmpdf = as.data.frame(matrix(FALSE, ncol = 4, nrow = nrow(df)))
+    fieldtypes = c('defaults', 'expand', 'multi', 'nested')
+    colnames(tmpdf) = fieldtypes
+    df = cbind(df,tmpdf)
+    for(i in fieldtypes) {
+        df[df$field  %in% json[[i]],i] = TRUE
+    }
+    return(df)
 }
