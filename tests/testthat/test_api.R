274d6cad2241ef2fbdb75fea3fdeb8d5dc25807e
Sean Davis <seandavi@gmail.com>
2017-01-07 13:00:44 -0500

    more tests

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -39,3 +39,22 @@ test_that("mapping", {
     expect_equal(ncol(res), 9)
     expect_equal(colnames(res),c('description','doc_type','field','full','type','defaults','expand','multi','nested'))
 })
+
+test_that("projects", {
+    res = projects()
+    expect_equal(class(res),'data.frame')
+    expect_equal(ncol(res), 7)
+    expect_equal(colnames(res),c('dbgap_accession_number','disease_type','released','state','primary_site','project_id','name'))
+})
+
+
+test_that('make_filter', {
+    res = make_filter('a'=='b',toJSON = FALSE)
+    expect_equal(class(res),'list')
+    expect_equal(names(res),c('op','content'))
+    expect_equal(res$op,jsonlite::unbox('='))
+    expect_equal(res$content$field,'a')
+    expect_equal(res$content$value,'b')
+})
+
+
