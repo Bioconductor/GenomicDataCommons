af677e58a1ddc748f9ee1d2d6fa73acb59d6d2a9
Sean Davis <seandavi@gmail.com>
2017-03-01 23:56:55 +0000

    added fill to rbindlist2 and doc changes

--- R/simplify_results.R
+++ R/simplify_results.R
@@ -1,6 +1,6 @@
 #' rbindlist, but with null values allowed
 #' @importFrom data.table rbindlist
-rbindlist2 = function(x,...) {rbindlist(Filter(Negate(is.null),x),...)}
+rbindlist2 = function(x,...) {rbindlist(Filter(Negate(is.null),x),...,fill=TRUE)}
 
 #' Convert GDC results to data.frame
 #'
@@ -9,9 +9,14 @@ rbindlist2 = function(x,...) {rbindlist(Filter(Negate(is.null),x),...)}
 #' R list structure to a data.frame. Some columns
 #' in the resulting data.frame may remain lists, but
 #' there will be one list element for each row though
-#' that list element may contain multiple values.  
+#' that list element may contain multiple values. 
+#' 
+#' @note The data.frame that is returned is the cartesian product 
+#' of sub-data.frames that come from the GDC. This may lead to more
+#' than one row per entity id. It is up to the user to determine 
+#' how to make rows unique per entity, if that is desired.
 #'
-#' @param res a \code{GDCResults} object to be converted to a 
+#' @param x a \code{GDCResults} object to be converted to a 
 #'     data.frame.
 #'
 #' @return a data.frame, potentially with list columns still
@@ -23,42 +28,42 @@ rbindlist2 = function(x,...) {rbindlist(Filter(Negate(is.null),x),...)}
 #' head(cases() %>% expand(expands) %>% as.data.frame())
 #' 
 #' @export
-as.data.frame.GDCResults <- function(res) {
+as.data.frame.GDCResults <- function(x, row.names, optional, ...) {
     # get the value of the id column from results
-    namecol = res[[id_field(res)]]
+    namecol = x[[id_field(x)]]
     
     # deal with list elements that are already data.frames
-    alreadydfs = lapply(Filter(is.data.frame,res),function(df) {
+    alreadydfs = lapply(Filter(is.data.frame,x),function(df) {
         df = cbind(data.frame(namecol),df)
-        colnames(df)[1] = id_field(res)
+        colnames(df)[1] = id_field(x)
         return(df)
     })
     
     # Deal with all vector columns by simply making a data
     # frame of them.
     # this will include the idfield, so no need to explicitly add
-    vecdf = as.data.frame(Filter(Negate(is.list),res))
+    vecdf = as.data.frame(Filter(Negate(is.list),x))
     
     # need the names for making data.frame columns,
     # so loop over names
-    listcolnames = Filter(function(n) {is.list(res[[n]]) & !is.data.frame(res[[n]])},names(res))
+    listcolnames = Filter(function(n) {is.list(x[[n]]) & !is.data.frame(x[[n]])},names(x))
     otherdfs = lapply(listcolnames,function(n) {
         tryCatch(
         expr = {
-            df = rbindlist2(res[[n]],idcol=id_field(res))
-            otherfields = !(colnames(df) %in% id_field(res))
+            df = rbindlist2(x[[n]],idcol=id_field(x))
+            otherfields = !(colnames(df) %in% id_field(x))
             colnames(df)[otherfields] = paste(n,colnames(df)[otherfields],sep='.')
             return(df)
         },
         error = function(e) {
-            df = data.frame(I(res[[n]]),namecol)
-            colnames(df) = c(n,id_field(res))
+            df = data.frame(I(x[[n]]),namecol)
+            colnames(df) = c(n,id_field(x))
             return(df)
         })
     })
     names(otherdfs) = listcolnames
     
-    Reduce(function(left,right) merge(left,right,all.x=TRUE,all.y=TRUE,by=id_field(res)),
+    Reduce(function(left,right) merge(left,right,all.x=TRUE,all.y=TRUE,by=id_field(x)),
            c(list(vecdf),otherdfs,alreadydfs))    
     
 }
