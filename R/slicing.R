104ba3c49e3cabf7e37ace4e245d5f65ce55cc88
Sean Davis <seandavi@gmail.com>
2021-08-09 13:49:06 -0600

    dontrun when gdc_token required

--- R/slicing.R
+++ R/slicing.R
@@ -42,7 +42,7 @@
 #' @importFrom jsonlite toJSON
 #' 
 #' @examples
-#' \donttest{
+#' \dontrun{
 #'  slicing("df80679e-c4d3-487b-934c-fcc782e5d46e",
 #'         regions="chr17:75000000-76000000",
 #'         token=gdc_token())
